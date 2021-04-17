c666784031b3dff2a44fb7d49c7425afdd251e42
Radha Satam <radha.satam@gmail.com>
2021-04-17 23:36:16 +0530

    Add react-geolocated, fetch coordinates and fetch state from coordinates (set in header)

--- src/context/StateContext.js
+++ src/context/StateContext.js
@@ -1,6 +1,43 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
+import { geolocated } from "react-geolocated"
+import { states } from "constant/states"
 
 export const StateContext = createContext()
+
+// Fetch latitude and longitude from browser api
+// Fetch current state from bigdatacloud free lookup api
+// Set selectedState in context
+const GeoLocation = geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})((props) => {
+  const { coords, setSelectedState } = props
+
+  useEffect(() => {
+    if (coords && coords.longitude && coords.latitude) {
+      fetch(
+        `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${coords.latitude}&longitude=${coords.longitude}&localityLanguage=en`
+      ).then((response) => {
+        console.log("response")
+        if (response) {
+          response.json().then((location) => {
+            if (
+              location &&
+              location.principalSubdivision &&
+              states.includes(location.principalSubdivision)
+            ) {
+              setSelectedState(location.principalSubdivision)
+            }
+          })
+        }
+      })
+    }
+  }, [coords, setSelectedState])
+
+  return null
+})
 
 // Permission Provider
 export default function StateContextWrapper({ children }) {
@@ -8,6 +45,7 @@ export default function StateContextWrapper({ children }) {
   // TODO: Add location based on the browser location as the default state - if possible
   return (
     <StateContext.Provider value={{ selectedState, setSelectedState }}>
+      <GeoLocation setSelectedState={setSelectedState} />
       {children}
     </StateContext.Provider>
   )
