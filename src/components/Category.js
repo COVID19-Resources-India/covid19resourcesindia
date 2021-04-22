be887b9f04e04cda21379083b4dc728a554c42c8
Guru107 <guruak107@gmail.com>
2021-04-22 17:59:43 +0530

    Add anchor tag for link column

--- src/components/Category.js
+++ src/components/Category.js
@@ -74,9 +74,11 @@ const CategoryComponent = ({ category, stateContext }) => {
         })
 
         let updatedColumns = columns
+        .map(i => i.key === "Link" ? {...i, render: (link) => <a href={link} target="_blank" rel="noreferrer">{link}</a>} : i )
+        
         if (!isExternalResources) {
           updatedColumns = [
-            ...columns,
+            ...updatedColumns,
             verificationColumn({
               upvote: upvoteFn,
               downvote: downvoteFn,
