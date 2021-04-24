9a39c73cc7ee6ef76c149c8b59124ab0a254e5e9
Jigar Mehta <jigar.1101@gmail.com>
2021-04-24 21:22:20 +0530

    Add twitter button and search

--- src/components/TwitterSearch.js
+++ src/components/TwitterSearch.js
@@ -1,36 +1,87 @@
+import { useState } from "react"
+import { Button, Input } from "antd"
+import Modal from "antd/lib/modal/Modal"
 import {
   TWITTER_DEFAULT_SEARCH_KEY,
   TWITTER_SEARCH_KEYS,
   TWITTER_SUBTRACT_SEARCH,
   TWITTER_VERIFIED_SEARCH,
 } from "constant/static"
+import "./TwitterSearch.scss"
+import { toTitleCase } from "utils/caseHelper"
 
 const mapCategoryToTwitterCategory = (category) =>
   TWITTER_SEARCH_KEYS[category] || TWITTER_DEFAULT_SEARCH_KEY
 
-const TwitterSearch = ({ stateContext, category }) => {
-  const { selectedState, selectedCity } = stateContext
+const TwitterSearch = ({ category }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [selectedCity, setSelectedCity] = useState("")
+  const mappedCategory = mapCategoryToTwitterCategory(category)
 
-  let searchString = TWITTER_VERIFIED_SEARCH
-
-  if (selectedCity) {
-    searchString += ` ${selectedCity}`
-  } else if (selectedState) {
-    searchString += ` ${selectedState}`
+  const showModal = () => {
+    setIsModalVisible(true)
   }
 
-  searchString += ` ${mapCategoryToTwitterCategory(category)}`
-  searchString += TWITTER_SUBTRACT_SEARCH
+  const handleOk = () => {
+    let searchString = TWITTER_VERIFIED_SEARCH
 
-  const twitterSearchUrl =
-    "https://twitter.com/search?q=" + encodeURIComponent(searchString)
+    if (selectedCity) {
+      searchString += ` ${selectedCity}`
+    }
+
+    searchString += ` ${mappedCategory}`
+    searchString += TWITTER_SUBTRACT_SEARCH
+
+    const twitterSearchUrl =
+      "https://twitter.com/search?q=" + encodeURIComponent(searchString)
+
+    window.open(twitterSearchUrl, "_blank").focus()
+
+    setIsModalVisible(false)
+  }
+
+  const handleCancel = () => {
+    setIsModalVisible(false)
+  }
+
+  const onInputChange = (event) => setSelectedCity(event.target.value)
 
   return (
-    <div className="twitter-search-url">
-      <a href={twitterSearchUrl} target="_blank" rel="noreferrer">
+    <>
+      <Button
+        className="search-twitter-button"
+        type="primary"
+        onClick={showModal}
+      >
         Search verified resources on Twitter
-      </a>
-    </div>
+      </Button>
+      <Modal
+        title="Search on Twitter"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        width={500}
+        okText="Search"
+      >
+        <div className="info-container">
+          <label className="info-label">City</label>
+          <Input
+            className="info-input"
+            required
+            value={selectedCity}
+            onChange={onInputChange}
+          />
+        </div>
+        <div className="info-container">
+          <label className="info-label">Category</label>
+          <Input
+            className="info-input"
+            value={toTitleCase(category)}
+            disabled
+          />
+        </div>
+      </Modal>
+    </>
   )
 }
 
