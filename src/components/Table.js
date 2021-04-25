db43e58513de5843c1cd4b08b39fdfee5cf8d143
Radha Satam <radha.satam@gmail.com>
2021-04-25 17:32:24 +0530

    Feature/search-params (#57)
    * Create SearchInput, useQuery helper, add / fetch search query params from url
    
    * Stop asking for geolocation
    
    * Add state to link params, update selectedState when different from query param, update on change selected state
    
    * Add state to external resources link redirect NoDataFound component
    
    * Open state selector by default on first load if selectedState is undefined
    
    * Remove unused dep react-geolocated
    
    * Yarn lock updated
    
    * minor

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,13 +1,35 @@
-import { useEffect, useState } from "react"
+import { useContext } from "react"
 // antd
-import { Empty, Input, Table as AntDTable } from "antd"
+import { Empty, Table as AntDTable } from "antd"
 // components
 import Loader from "components/Loader"
-// icons
-import { ReactComponent as SearchIcon } from "assets/icons/search.svg"
+import SearchInput from "components/SearchInput"
 // styles
 import "./Table.scss"
 import { Link } from "react-router-dom"
+// utils
+import { useQuery } from "utils/hooksHelper"
+import { toKebabCase } from "utils/caseHelper"
+// context
+import { StateContext } from "context/StateContext"
+
+const NoDataFound = () => {
+  const { selectedState } = useContext(StateContext)
+  return (
+    <Empty
+      className="empty-table"
+      description={
+        <span>
+          Sorry, we're still gathering sources for this category in your region.
+          Meanwhile, you can check if there's something in&nbsp;
+          <Link to={`/search/${toKebabCase(selectedState)}/external-resources`}>
+            External Resources.
+          </Link>
+        </span>
+      }
+    />
+  )
+}
 
 // Accepts datasource and columns and displays in table format
 const Table = ({
@@ -17,13 +39,13 @@ const Table = ({
   loading = false,
   error = false,
   heading = "",
-  resetSearch = false,
 }) => {
-  const [searchedValue, setSearchedValue] = useState("")
-  const [filteredData, setFilteredData] = useState([])
+  const query = useQuery()
+  const searchTerm = query.get("q")
 
-  // Search Function
-  useEffect(() => {
+  let filteredData = []
+  // Filter data based on the query search term
+  if (searchTerm) {
     const newFilteredData = dataSource?.filter((entry) => {
       // Creating new object as the key can be deleted only in new object otherwise it'll delete from reference
       const newEntry = { ...entry }
@@ -32,22 +54,13 @@ const Table = ({
       const values = Object.values(newEntry)
       return values.find((value = "") =>
         // Looks like in some cases the data was having value as number
-        value?.toString().toLowerCase().includes(searchedValue.toLowerCase())
+        value?.toString().toLowerCase().includes(searchTerm.toLowerCase())
       )
     })
-    if (searchedValue) setFilteredData(newFilteredData)
-    else setFilteredData(dataSource)
-  }, [dataSource, searchedValue])
-
-  useEffect(() => {
-    // If datasource changes i.e routes change, reset the search in case of external resources so that the empty box is not seen in that case.
-    if (resetSearch) {
-      setSearchedValue("")
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dataSource])
-
-  const onSearchChange = (e) => setSearchedValue(e.target.value)
+    filteredData = newFilteredData
+  } else {
+    filteredData = dataSource
+  }
 
   if (loading) {
     return <Loader />
@@ -63,13 +76,7 @@ const Table = ({
 
   return (
     <div className="query-result">
-      <Input
-        className="filter-box"
-        placeholder="Start typing here to filter results..."
-        suffix={<SearchIcon />}
-        onChange={onSearchChange}
-        value={searchedValue}
-      />
+      <SearchInput searchTermFromQuery={searchTerm} />
       <AntDTable
         columns={columns}
         dataSource={filteredData}
@@ -79,20 +86,7 @@ const Table = ({
           position: ["topCenter", "bottomCenter"],
         }}
         locale={{
-          emptyText: (
-            <Empty
-              className="empty-table"
-              description={
-                <span>
-                  Sorry, we're still gathering sources for this category in your
-                  region. Meanwhile, you can check if there's something in&nbsp;
-                  <Link to="/search/external-resources">
-                    External Resources.
-                  </Link>
-                </span>
-              }
-            />
-          ),
+          emptyText: <NoDataFound />,
         }}
       />
     </div>
