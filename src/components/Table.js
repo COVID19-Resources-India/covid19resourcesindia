92c698cdd22801cb190a7930536b4a8befb54c45
Radha Satam <radha.satam@gmail.com>
2021-04-25 17:08:46 +0530

    Add state to external resources link redirect NoDataFound component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,3 +1,4 @@
+import { useContext } from "react"
 // antd
 import { Empty, Table as AntDTable } from "antd"
 // components
@@ -8,6 +9,27 @@ import "./Table.scss"
 import { Link } from "react-router-dom"
 // utils
 import { useQuery } from "utils/hooksHelper"
+import { toKebabCase } from "utils/caseHelper"
+// context
+import { StateContext } from "context/StateContext"
+
+const NoDataFound = () => {
+  const { selectedState } = useContext(StateContext)
+  return (
+    <Empty
+      className="empty-table"
+      description={
+        <span>
+          Sorry, we're still gathering sources for this category in your region.
+          Meanwhile, you can check if there's something in&nbsp;
+          <Link to={`/search/${toKebabCase(selectedState)}/external-resources`}>
+            External Resources.
+          </Link>
+        </span>
+      }
+    />
+  )
+}
 
 // Accepts datasource and columns and displays in table format
 const Table = ({
@@ -64,20 +86,7 @@ const Table = ({
           position: ["topCenter", "bottomCenter"],
         }}
         locale={{
-          emptyText: (
-            <Empty
-              className="empty-table"
-              description={
-                <span>
-                  Sorry, we're still gathering sources for this category in your
-                  region. Meanwhile, you can check if there's something in&nbsp;
-                  <Link to="/search/external-resources">
-                    External Resources.
-                  </Link>
-                </span>
-              }
-            />
-          ),
+          emptyText: <NoDataFound />,
         }}
       />
     </div>
