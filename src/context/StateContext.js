76b87708e1bd586168621ca481fcf8db6d42d1d4
Radha Satam <radha.satam@gmail.com>
2021-04-25 14:44:06 +0530

    Stop asking for geolocation

--- src/context/StateContext.js
+++ src/context/StateContext.js
@@ -1,80 +1,13 @@
-import { createContext, useEffect, useState } from "react"
-import { geolocated } from "react-geolocated"
-// constant
-import { states } from "constant/states"
+import { createContext, useState } from "react"
 
 export const StateContext = createContext()
-
-// Fetch latitude and longitude from browser api
-// Fetch current state from bigdatacloud free lookup api
-// Set selectedState in context
-const GeoLocation = geolocated({
-  positionOptions: {
-    enableHighAccuracy: false,
-  },
-  userDecisionTimeout: 5000,
-})((props) => {
-  const {
-    coords,
-    isGeolocationAvailable,
-    isGeolocationEnabled,
-    setLoadingState,
-    setSelectedState,
-  } = props
-  // Stops loading if geo location not supported in browser
-  useEffect(() => {
-    if (!isGeolocationAvailable) {
-      setLoadingState(false)
-    }
-  }, [isGeolocationAvailable, setLoadingState])
-
-  // Stops loading if geo location permission rejected by user
-  useEffect(() => {
-    if (!isGeolocationEnabled) {
-      setLoadingState(false)
-    }
-  }, [isGeolocationEnabled, setLoadingState])
-
-  useEffect(() => {
-    if (
-      isGeolocationAvailable &&
-      coords &&
-      coords.longitude &&
-      coords.latitude
-    ) {
-      setLoadingState(true)
-      fetch(
-        `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${coords.latitude}&longitude=${coords.longitude}&localityLanguage=en`
-      )
-        .then((response) => {
-          if (response) {
-            response.json().then((location) => {
-              const currentState = location?.principalSubdivision
-              if (currentState && states.includes(currentState)) {
-                setSelectedState(currentState)
-                setLoadingState(false)
-              }
-            })
-          }
-        })
-        .catch((response) => {
-          setLoadingState(false)
-        })
-    }
-  }, [coords, isGeolocationAvailable, setLoadingState, setSelectedState])
-
-  return null
-})
 
 // Permission Provider
 export default function StateContextWrapper({ children }) {
   const persistedState = localStorage.getItem("state")
   const [selectedState, setSelectedState] = useState(persistedState)
-  const [loadingState, setLoadingState] = useState(
-    persistedState ? false : true
-  )
+
   const contextValues = {
-    loadingState,
     selectedState,
     setSelectedState: (v) => {
       localStorage.setItem("state", v)
@@ -84,12 +17,6 @@ export default function StateContextWrapper({ children }) {
 
   return (
     <StateContext.Provider value={contextValues}>
-      {!persistedState && (
-        <GeoLocation
-          setSelectedState={contextValues.setSelectedState}
-          setLoadingState={setLoadingState}
-        />
-      )}
       {children}
     </StateContext.Provider>
   )
