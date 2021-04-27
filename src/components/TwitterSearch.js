7965f63493b199311b0c5e2890f2710237bcdcfc
Radha Satam <radha.satam@gmail.com>
2021-04-27 13:28:33 +0530

    Get category from location and display in twitter search box

--- src/components/TwitterSearch.js
+++ src/components/TwitterSearch.js
@@ -1,61 +1,78 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { useLocation } from "react-router-dom"
 // antd
 import { Button, Input, Select } from "antd"
 import Modal from "antd/lib/modal/Modal"
 // constant
 import {
+  TWITTER_ADDITIONAL_CATEGORIES,
   TWITTER_DEFAULT_SEARCH_KEY,
   TWITTER_SEARCH_KEYS,
   TWITTER_SUBTRACT_SEARCH,
   TWITTER_VERIFIED_SEARCH,
+  CATEGORIES,
 } from "constant/static"
+import { toKebabCase } from "utils/caseHelper"
+import { usePrevious } from "utils/hooksHelper"
 // icons
 import { ReactComponent as TwitterIcon } from "assets/icons/twitter.svg"
 // styles
 import "./TwitterSearch.scss"
 
 const { Option } = Select
-
-// const mapCategoryToTwitterCategory = (category) =>
-//   TWITTER_SEARCH_KEYS.find((element) => element.key === category)
-
 const categoryOptions = []
-for (let i = 0; i < TWITTER_SEARCH_KEYS.length; i++) {
-  categoryOptions.push(
-    <Option
-      key={TWITTER_SEARCH_KEYS[i].key}
-      value={TWITTER_SEARCH_KEYS[i].value}
-    >
-      {TWITTER_SEARCH_KEYS[i].label}
-    </Option>
-  )
+const categoriesToUse = [...CATEGORIES, ...TWITTER_ADDITIONAL_CATEGORIES]
+for (let i = 0; i < categoriesToUse.length; i++) {
+  if (categoriesToUse[i] !== "External Resources") {
+    const value = toKebabCase(categoriesToUse[i])
+    categoryOptions.push(
+      <Option key={value} value={value}>
+        {categoriesToUse[i]}
+      </Option>
+    )
+  }
 }
 
 const TwitterSearch = () => {
+  // Fetching category from location
+  // useParams not available here because this component is outside of the switch
+  let categoryFromLocation = undefined
+  const location = useLocation()
+  const splitLocation = location.pathname.split("/")
+  if (
+    splitLocation[1] === "search" &&
+    splitLocation[3] &&
+    splitLocation[3] !== "external-resources"
+  ) {
+    categoryFromLocation = splitLocation[3]
+  }
+  const prevCategory = usePrevious(categoryFromLocation)
+
   const [isModalVisible, setIsModalVisible] = useState(false)
   const [selectedCity, setSelectedCity] = useState("")
 
-  // TODO: Figure out a way to remember the existing categories and url category
-  // Cases
-  // 1. If user lands on a category page and seaches
-  // 2. If user lands on a category page and moves on different category and searches
-  // 3. If user lands on a non category page and searches
-  // 4. If user langs on a category page, adds another category, goes on a different category and searches
-  // 5. If a user lands on a non category page, adds a category and goes to a category page and searches
-  // So as of now not selecting any category by default.
-
-  // const mappedCategory = mapCategoryToTwitterCategory(category)
   const [categoryValue, setCategoryValue] = useState(
-    []
-    // mappedCategory && mappedCategory.value
-    //   ? [mappedCategory.value]
-    //   : TWITTER_DEFAULT_SEARCH_KEY
+    categoryFromLocation ? [categoryFromLocation] : []
   )
 
-  const showModal = () => {
-    setIsModalVisible(true)
-  }
+  // TODO:
+  // 4. If user lands on a category page, adds another category, goes on a different category and searches
+  // 5. If a user lands on a non category page, adds a category and goes to a category page and searches
+  // (Right now the category is reset every time the route changes)
 
+  // set the category from location on route change
+  useEffect(() => {
+    if (prevCategory !== categoryFromLocation) {
+      setCategoryValue((p) =>
+        categoryFromLocation ? [categoryFromLocation] : []
+      )
+    }
+  }, [prevCategory, categoryFromLocation, setCategoryValue])
+
+  const showModal = () => setIsModalVisible(true)
+  const handleCancel = () => setIsModalVisible(false)
+  const handleChange = (value) => setCategoryValue(value)
+  const onInputChange = (event) => setSelectedCity(event.target.value)
   const handleOk = () => {
     let searchString = TWITTER_VERIFIED_SEARCH
 
@@ -65,7 +82,7 @@ const TwitterSearch = () => {
 
     searchString += ` ${
       categoryValue && categoryValue.length
-        ? categoryValue.join(" OR ")
+        ? categoryValue.map((i) => TWITTER_SEARCH_KEYS[i]).join(" OR ")
         : TWITTER_DEFAULT_SEARCH_KEY
     }`
     searchString += TWITTER_SUBTRACT_SEARCH
@@ -77,16 +94,6 @@ const TwitterSearch = () => {
 
     setIsModalVisible(false)
   }
-
-  const handleCancel = () => {
-    setIsModalVisible(false)
-  }
-
-  const handleChange = (value) => {
-    setCategoryValue(value)
-  }
-
-  const onInputChange = (event) => setSelectedCity(event.target.value)
 
   return (
     <>
--- src/constant/static.js
+++ src/constant/static.js
@@ -7,35 +7,15 @@ const CATEGORIES = [
   "External Resources",
 ]
 
+const TWITTER_ADDITIONAL_CATEGORIES = ["Plasma"]
 const TWITTER_DEFAULT_SEARCH_KEY = "(bed OR beds OR oxygen)"
-
-const TWITTER_SEARCH_KEYS = [
-  {
-    key: "beds",
-    value: "(bed OR beds)",
-    label: "Beds",
-  },
-  {
-    key: "oxygen-cylinders",
-    value: "oxygen",
-    label: "Oxygen Cylinders",
-  },
-  {
-    key: "remdesivir",
-    value: "remdesivir",
-    label: "Remdesivir",
-  },
-  {
-    key: "tocilizumab",
-    value: "tocilizumab",
-    label: "Tocilizumab",
-  },
-  {
-    key: "plasma",
-    value: "plasma",
-    label: "Plasma",
-  },
-]
+const TWITTER_SEARCH_KEYS = {
+  beds: "(bed OR beds)",
+  "oxygen-cylinders": "oxygen",
+  remdesivir: "remdesivir",
+  tocilizumab: "tocilizumab",
+  plasma: "plasma",
+}
 
 const TWITTER_SUBTRACT_SEARCH = ` -"not verified" -"unverified" -"needed" -"required" -"requirement" -"not available" -"unavailable" -"not working"`
 
@@ -44,6 +24,7 @@ const TWITTER_VERIFIED_SEARCH = "(verified OR available OR working)"
 export {
   CATEGORIES,
   SPREADSHEET_KEY,
+  TWITTER_ADDITIONAL_CATEGORIES,
   TWITTER_DEFAULT_SEARCH_KEY,
   TWITTER_SEARCH_KEYS,
   TWITTER_SUBTRACT_SEARCH,
