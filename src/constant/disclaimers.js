ee0e8c618d1e33e57e734591648106be1926422a
Radha Satam <radha.satam@gmail.com>
2021-05-01 19:38:46 +0530

    Update disclaimer text

--- src/constant/disclaimers.js
+++ src/constant/disclaimers.js
@@ -1,39 +1,65 @@
+import { Link } from "react-router-dom"
+
+const sourcesLink = (
+  <div>
+    👥 &nbsp;These records are crowd-sourced from the internet and have{" "}
+    <strong>not</strong> been verified by us. You can check{" "}
+    <Link to="/sources">our sources here</Link>.
+  </div>
+)
+
+const upvoteRequest = (
+  <div>
+    👍 &nbsp;Please <strong>upvote / downvote</strong> if you found it useful or
+    not. This will help others.
+  </div>
+)
+
+const scamDisclaimer = (
+  <div>
+    ❗ &nbsp;There have been reports of <strong>scams</strong> and black market
+    transactions. Please be careful. It might be a scam if they are asking to
+    pre-pay.
+  </div>
+)
+
 const DISCLAIMERS = {
   remdesivir: (
     <>
       <div>
-        Please note that Remdesivir is not a life saving drug for COVID-19. It
-        is to be administered in hospital settings only.
+        💊 &nbsp;Please note that Remdesivir is not a life saving drug for
+        COVID-19. It is to be administered in hospitals only.
       </div>
+      <br />
       <div>
-        ❗ According to government guidelines Remdesivir can only be sold to
-        private and state COVID hospitals.
+        ℹ️ &nbsp;According to government guidelines Remdesivir can{" "}
+        <strong>only be sold to private and state COVID hospitals</strong>.
+        Please reach out to your district collectors office as they can send the
+        medicine to the hospital.
       </div>
-      <div>
-        There have been reports of scams. Please exercise caution. Please report
-        scams to the cybercell.
-      </div>
+      <br />
+      {scamDisclaimer}
     </>
   ),
   tocilizumab: (
     <>
-      <div>
-        ❗ According to government guidelines Tocilizumab can only be sold to
-        private and state COVID hospitals.
-      </div>
-      <div>There have been reports of scams. Please exercise caution.</div>
+      {sourcesLink}
+      <br />
+      {scamDisclaimer}
+      <br />
+      {upvoteRequest}
     </>
   ),
   "oxygen-cylinders": (
     <>
-      <div>
-        These sources are crowd-sourced and have not been verified by us. You
-        can check the sources page.
-      </div>
-      {/* <div>Do upvote / downvote.</div> */}
-      <div>There have been reports of scams. Please exercise caution.</div>
+      {sourcesLink}
+      <br />
+      {scamDisclaimer}
+      <br />
+      {upvoteRequest}
     </>
   ),
+  beds: <>{upvoteRequest}</>,
 }
 
 export { DISCLAIMERS }
