5b86a493369043e13e51f15e15bbd07191424f8b
Radha Satam <radha.satam@gmail.com>
2021-05-06 16:38:16 +0530

    Add ambulance category

--- src/constant/static.js
+++ src/constant/static.js
@@ -1,11 +1,12 @@
 const SPREADSHEET_KEY = "11cAlFOEgfIyE0jmV_8X8_DqImCfxHyk7zo8wETFX9x4"
 const CATEGORIES = [
+  "Ambulance",
   "Beds",
+  "External Resources",
   "Oxygen Cylinders",
+  "Plasma",
   "Remdesivir",
   "Tocilizumab",
-  "Plasma",
-  "External Resources",
 ]
 
 const CATEGORIES_WITHOUT_VERIFICATION_COLUMN = ["external-resources", "plasma"]
@@ -14,6 +15,7 @@ const CATEGORIES_WITH_NATIONAL = ["external-resources", "plasma"]
 const TWITTER_ADDITIONAL_CATEGORIES = []
 const TWITTER_DEFAULT_SEARCH_KEY = "(bed OR beds OR oxygen)"
 const TWITTER_SEARCH_KEYS = {
+  ambulance: "ambulance",
   beds: "(bed OR beds)",
   "oxygen-cylinders": "oxygen",
   remdesivir: "remdesivir",
@@ -34,5 +36,5 @@ export {
   TWITTER_SUBTRACT_SEARCH,
   TWITTER_VERIFIED_SEARCH,
   CATEGORIES_WITHOUT_VERIFICATION_COLUMN,
-  CATEGORIES_WITH_NATIONAL
+  CATEGORIES_WITH_NATIONAL,
 }
